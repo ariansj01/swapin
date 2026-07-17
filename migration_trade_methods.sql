561a125a0ecfd8b25d0d079c695d53acbe5ddfd3
ariansj01 <ariansj.ir@gmail.com>
2026-07-17 12:45:06 +0330

    chore: remove sensitive production files from web root

--- migration_trade_methods.sql
+++
@@ -1,4 +0,0 @@
--- Migration to add selected payment and shipping methods to trades table
-ALTER TABLE `trades` 
-ADD COLUMN `selected_payment_method` enum('in_person','bnpl','cash') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Selected payment method for price difference',
-ADD COLUMN `selected_shipping_method` enum('courier','post','swapin_secure') COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Selected shipping method for the trade';
