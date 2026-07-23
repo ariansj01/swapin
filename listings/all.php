21cfc3ed376e1f94931530b16f880a648370c9ed
ariansj01 <ariansj.ir@gmail.com>
2026-07-24 01:19:35 +0330

    increase width for mobile card

--- listings/all.php
+++ listings/all.php
@@ -254,7 +254,7 @@ render_navbar($user);
       <?php else: ?>
       <div class="all-listings-grid">
         <?php foreach ($listings as $l): ?>
-        <div class="all-page-listing-card" style="width: 150%;">
+        <div class="all-page-listing-card">
           <?php include __DIR__ . '/../includes/listing_card.php'; ?>
         </div>
         <?php endforeach; ?>
