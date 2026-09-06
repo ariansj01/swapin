b791b5180a0ea56b8fc54c7e7ecffceef9201614
ariansj01 <ariansj.ir@gmail.com>
2026-09-06 12:27:43 +0330

    deploy test

--- pwa.php
+++ pwa.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/layout.php';
 
 $user = auth_user();
- 
+
 render_head(
     'نصب اپلیکیشن سواَپین',
     'نصب اپلیکیشن سواَپین',
